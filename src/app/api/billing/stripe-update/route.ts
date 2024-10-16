5590e15835e8f78909040a596ac3026ede232b0c
marouaneebd <marouaneebd@gmail.com>
2024-10-16 21:19:21 +0200

    Plan update

--- src/app/api/billing/stripe-update/route.ts
+++ src/app/api/billing/stripe-update/route.ts
@@ -4,35 +4,44 @@ import { doc, getDocs, updateDoc, query, collection, where } from 'firebase/fire
 import Stripe from 'stripe';
 import { NextRequest, NextResponse } from 'next/server';
 
+const price_ids = {
+  "assisted": "price_1Q2eySIHyU82otGEK3Te8zGb",
+  "augmented": "price_1Q2eywIHyU82otGEGEYwpbyh",
+  "automated": "price_1Q2ezLIHyU82otGExUGl6dP9"
+};
+
 export async function POST(req: NextRequest) {
-  //const apiKey = req.headers.get('x-api-key');
-  //const validApiKey = process.env.STRIPE_WEBHOOK_API_KEY;
-
-  //if (!apiKey || apiKey !== validApiKey) {
-    //return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
-  //}
-
-
   let event: Stripe.Event;
 
   try {
     const buf = await req.arrayBuffer();
     const body = Buffer.from(buf);
-    
-    // Proceed with the rest of your code
     const signature = req.headers.get('stripe-signature') as string;
     event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET as string);
-    
   } catch (err) {
     return NextResponse.json({ error: `Webhook Error: ${JSON.stringify(err)}` }, { status: 400 });
   }
 
   const subscription = event.data.object as Stripe.Subscription;
 
-  console.log(event)
-
   if (event.type === 'customer.subscription.updated') {
     const customerId = subscription.customer as string;
+    const priceId = subscription.items.data[0].price.id;
+    const isTrial = subscription.status === 'trialing'; // Check if the subscription is in trial
+
+    let plan: string | null = null;
+
+    // Match the priceId with the appropriate plan
+    for (const [key, value] of Object.entries(price_ids)) {
+      if (value === priceId) {
+        plan = key;
+        break;
+      }
+    }
+
+    if (!plan) {
+      return NextResponse.json({ error: 'Invalid price ID' }, { status: 400 });
+    }
 
     try {
       const profilesRef = collection(db, 'profiles');
@@ -46,7 +55,8 @@ export async function POST(req: NextRequest) {
       for (const document of querySnapshot.docs) {
         const docRef = doc(db, 'profiles', document.id);
         await updateDoc(docRef, {
-          plan: "new"
+          plan: plan,
+          isTrial: isTrial // Add the trial status to the user's profile
         });
       }
 
