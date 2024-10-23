81c32f31c457c630a11150837555a09d46a03936
marouaneebd <marouaneebd@gmail.com>
2024-10-23 22:28:01 +0200

    Fix template error

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,29 +4,21 @@ import { db } from '@/app/firebase';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { getServerSession } from 'next-auth/next';
 
-export async function GET(req: NextRequest) {
+
+export async function POST(req: NextRequest) {
     try {
-        // Retrieve query parameters from the URL
-        const templateId = req.nextUrl.searchParams.get('templateId');
-        const targetFirstName = req.nextUrl.searchParams.get('targetFirstName');
-        const targetLastName = req.nextUrl.searchParams.get('targetLastName');
-        const targetCompany = req.nextUrl.searchParams.get('targetCompany');
-
-        // Get session details
+        // Parse the request body
+        const { templateId, targetFirstName, targetLastName, targetCompany, } = await req.json();
         const session = await getServerSession({ req, ...authOptions });
-        const uid = session?.user?.uid;
+        const uid = session?.user?.uid
 
-        // Check if the user is logged in
         if (!uid) {
             return NextResponse.json({ error: 'Unauthorized. Please log in.' }, { status: 401 });
         }
 
-        // Validate required templateId parameter
         if (!templateId) {
             return NextResponse.json({ error: 'Template ID is required' }, { status: 400 });
         }
-        
-        console.log(uid)
 
         const templateRef = doc(db, 'templates', templateId);
         const templateSnap = await getDoc(templateRef);
@@ -34,46 +26,43 @@ export async function GET(req: NextRequest) {
         const profileRef = doc(db, 'profiles', uid);
         const profileSnap = await getDoc(profileRef);
 
-
-
         if (profileSnap.exists() && templateSnap.exists()) {
             const profileData = profileSnap.data();
             const templateData = templateSnap.data();
 
             const plan = profileData?.plan;
+
             const currentDate = new Date(new Date().setHours(0, 0, 0, 0));
             const dateCreditsRefreshed = profileData?.dateCreditsRefreshed;
 
             const reinitializeCredits = currentDate === dateCreditsRefreshed;
             const creditsUsed = reinitializeCredits ? 0 : profileData?.creditsUsed;
 
-            const remainingCredits =
-                (plan === 'assisted' ? 10 : plan === 'augmented' ? 15 : plan === 'automated' ? 20 : 0) - creditsUsed;
+            const remainingCredits = (plan === "assisted" ? 10 : plan === "augmented" ? 15 : plan === "automated" ? 20 : 0) - creditsUsed;
 
-            // Check if user has an active plan and remaining credits
             if (profileData?.isActive && remainingCredits > 0) {
                 let message = templateData?.templateContent;
-                message = message.replaceAll('[firstname]', targetFirstName || '');
-                message = message.replaceAll('[lastname]', targetLastName || '');
-                message = message.replaceAll('[company]', targetCompany || '');
+                message.replaceAll('[firstname]', targetFirstName);
+                message.replaceAll('[lastname]', targetLastName);
+                message.replaceAll('[company]', targetCompany);
 
-                // Update the credits used
                 if (reinitializeCredits) {
                     await updateDoc(profileRef, {
                         creditsUsed: 1,
                         dateCreditsRefreshed: new Date(new Date().setHours(0, 0, 0, 0)),
-                        updatedAt: new Date().getTime().toString(),
-                    });
-                } else {
-                    await updateDoc(profileRef, {
-                        creditsUsed: creditsUsed + 1,
-                        updatedAt: new Date().getTime().toString(),
+                        updatedAt: new Date().getTime().toString()
                     });
                 }
-
+                else {
+                    await updateDoc(profileRef, {
+                        creditsUsed: creditsUsed + 1,
+                        updatedAt: new Date().getTime().toString()
+                    });
+                }
+                
                 return NextResponse.json({ message });
             } else {
-                return NextResponse.json({ error: 'Plan not active or no remaining credits' }, { status: 400 });
+                return NextResponse.json({ error: 'Plan not active' }, { status: 400 });
             }
         } else {
             return NextResponse.json({ error: 'Profile or template not found' }, { status: 404 });
