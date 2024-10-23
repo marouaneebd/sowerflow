7f9153300b9cfc713ed1aa1e11e179fd616da5d6
marouaneebd <marouaneebd@gmail.com>
2024-10-23 21:56:44 +0200

    Fix template error

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,93 +4,7 @@ import { db } from '@/app/firebase';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { getServerSession } from 'next-auth/next';
 
-export async function OPTIONS() {
-    const headers = {
-        'Access-Control-Allow-Origin': '*', // Replace '*' with your specific origin if needed
-        'Access-Control-Allow-Methods': 'POST, OPTIONS'
-    };
-    return NextResponse.json({}, { headers });
-}
-
-export async function POST(req: NextRequest) {
-    const headers = {
-        'Access-Control-Allow-Origin': '*', // Replace '*' with your specific origin if needed
-    };
-    try {
-
-        // Parse the request body
-        const { templateId, targetFirstName, targetLastName, targetCompany } = await req.json();
-        const session = await getServerSession({ req, ...authOptions });
-        const uid = session?.user?.uid;
-
-        console.log(templateId, targetFirstName);
-
-        if (!uid) {
-            return NextResponse.json({ error: 'Unauthorized. Please log in.' }, { status: 401, headers });
-        }
-
-        if (!templateId) {
-            return NextResponse.json({ error: 'Template ID is required' }, { status: 400, headers });
-        }
-
-        const templateRef = doc(db, 'templates', templateId);
-        const templateSnap = await getDoc(templateRef);
-
-        const profileRef = doc(db, 'profiles', uid);
-        const profileSnap = await getDoc(profileRef);
-
-        if (profileSnap.exists() && templateSnap.exists()) {
-            console.log('profile and template found');
-            const profileData = profileSnap.data();
-            const templateData = templateSnap.data();
-
-            const plan = profileData?.plan;
-
-            const currentDate = new Date(new Date().setHours(0, 0, 0, 0));
-            const dateCreditsRefreshed = profileData?.dateCreditsRefreshed;
-
-            const reinitializeCredits = currentDate === dateCreditsRefreshed;
-            const creditsUsed = reinitializeCredits ? 0 : profileData?.creditsUsed;
-
-            const remainingCredits =
-                (plan === 'assisted' ? 10 : plan === 'augmented' ? 15 : plan === 'automated' ? 20 : 0) - creditsUsed;
-
-            if (profileData?.isActive && remainingCredits > 0) {
-                let message = templateData?.messageContent;
-                message = message.replaceAll('[firstname]', targetFirstName);
-                message = message.replaceAll('[lastname]', targetLastName);
-                message = message.replaceAll('[company]', targetCompany);
-
-                if (reinitializeCredits) {
-                    await updateDoc(profileRef, {
-                        creditsUsed: 1,
-                        dateCreditsRefreshed: new Date(new Date().setHours(0, 0, 0, 0)),
-                        updatedAt: new Date().getTime().toString(),
-                    });
-                } else {
-                    await updateDoc(profileRef, {
-                        creditsUsed: creditsUsed + 1,
-                        updatedAt: new Date().getTime().toString(),
-                    });
-                }
-
-                return NextResponse.json({ message }, { headers });
-            } else {
-                return NextResponse.json({ error: 'Plan not active' }, { status: 400, headers });
-            }
-        } else {
-            return NextResponse.json({ error: 'Profile or template not found' }, { status: 404, headers });
-        }
-    } catch (error) {
-        console.error('Error creating message:', error);
-        return NextResponse.json({ error: 'Failed to create message' }, { status: 500, headers });
-    }
-}
-
 export async function GET(req: NextRequest) {
-    const headers = {
-        'Access-Control-Allow-Origin': '*', // Replace '*' with your specific origin if needed
-    };
     try {
         // Retrieve query parameters from the URL
         const templateId = req.nextUrl.searchParams.get('templateId');
@@ -102,24 +16,26 @@ export async function GET(req: NextRequest) {
         const session = await getServerSession({ req, ...authOptions });
         const uid = session?.user?.uid;
 
-        console.log(templateId, targetFirstName);
-
         // Check if the user is logged in
         if (!uid) {
-            return NextResponse.json({ error: 'Unauthorized. Please log in.' }, { status: 401, headers });
+            return NextResponse.json({ error: 'Unauthorized. Please log in.' }, { status: 401 });
         }
 
         // Validate required templateId parameter
         if (!templateId) {
-            return NextResponse.json({ error: 'Template ID is required' }, { status: 400, headers });
+            return NextResponse.json({ error: 'Template ID is required' }, { status: 400 });
         }
 
-        // Fetch template and profile data from Firestore
+        console.log("ok 1")
+
         const templateRef = doc(db, 'templates', templateId);
         const templateSnap = await getDoc(templateRef);
 
         const profileRef = doc(db, 'profiles', uid);
         const profileSnap = await getDoc(profileRef);
+
+        console.log("ok 2")
+
 
         if (profileSnap.exists() && templateSnap.exists()) {
             console.log('Profile and template found');
@@ -157,15 +73,15 @@ export async function GET(req: NextRequest) {
                     });
                 }
 
-                return NextResponse.json({ message }, { headers });
+                return NextResponse.json({ message });
             } else {
-                return NextResponse.json({ error: 'Plan not active or no remaining credits' }, { status: 400, headers });
+                return NextResponse.json({ error: 'Plan not active or no remaining credits' }, { status: 400 });
             }
         } else {
-            return NextResponse.json({ error: 'Profile or template not found' }, { status: 404, headers });
+            return NextResponse.json({ error: 'Profile or template not found' }, { status: 404 });
         }
     } catch (error) {
         console.error('Error creating message:', error);
-        return NextResponse.json({ error: 'Failed to create message' }, { status: 500, headers });
+        return NextResponse.json({ error: 'Failed to create message' }, { status: 500 });
     }
 }
