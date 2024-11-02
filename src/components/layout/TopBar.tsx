f4ab677dd40e5ddf24d3b3326fc45856f0adf6fc
marouaneebd <marouaneebd@gmail.com>
2024-11-02 23:28:50 +0100

    Add top bar

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { signOut, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { useState, useEffect } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function TopBar() {
     const router = useRouter();
